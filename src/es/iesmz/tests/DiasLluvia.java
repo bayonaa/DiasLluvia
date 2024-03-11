3e79fcae2f9ed9364bf84044a3f33272616ad04f
bayonaa <mbayona03@gmail.com>
2024-03-11 19:55:47 +0100

    Método consultarDia() terminado

--- src/es/iesmz/tests/DiasLluvia.java
+++ src/es/iesmz/tests/DiasLluvia.java
@@ -15,4 +15,12 @@ public class DiasLluvia {
         registroDiasLluvia[mes - 1][dia - 1] = lluvia;
         return true;
     }
+
+    public boolean consultarDia(int dia, int mes) {
+        if (mes < 1 || mes > 12 || dia < 1 || dia > 31) {
+            System.out.println("Mes o día inválido.");
+            return false;
+        }
+        return registroDiasLluvia[mes - 1][dia - 1];
+    }
 }
