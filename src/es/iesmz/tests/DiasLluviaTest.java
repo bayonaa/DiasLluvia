7548ae82425bd57dee21bab1e8af571ba7a70a67
bayonaa <mbayona03@gmail.com>
2024-03-11 20:00:53 +0100

    Tests para el método registroDia()

--- src/es/iesmz/tests/DiasLluviaTest.java
+++ src/es/iesmz/tests/DiasLluviaTest.java
@@ -1,4 +1,30 @@
 package es.iesmz.tests;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DiasLluviaTest {
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("EXECUTE PROVA");
+    }
+
+    @Test //Test1 para el método registroDia()
+    public void testRegistroDia1() {
+        DiasLluvia diasLluvia = new DiasLluvia();
+        assertTrue(diasLluvia.registroDia(1, 1, true));
+    }
+
+    @Test //Test2 para el método registroDia()
+    public void testRegistroDia2() {
+        DiasLluvia diasLluvia = new DiasLluvia();
+        assertTrue(diasLluvia.registroDia(15, 6, false));
+    }
+
+    @Test //Test3 para el método registroDia()
+    public void testRegistroDia3() {
+        DiasLluvia diasLluvia = new DiasLluvia();
+        assertFalse(diasLluvia.registroDia(32, 12, true));
+    }
 }
