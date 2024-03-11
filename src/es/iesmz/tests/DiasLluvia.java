0453322b6f6e6c23085ce79c53bbb34bbe5324c0
bayonaa <mbayona03@gmail.com>
2024-03-11 19:57:06 +0100

    Método trimestreLluvioso() terminado

--- src/es/iesmz/tests/DiasLluvia.java
+++ src/es/iesmz/tests/DiasLluvia.java
@@ -35,4 +35,35 @@ public class DiasLluvia {
         }
         return  cont;
     }
+
+    public int trimestreLluvioso() {
+        int[] trimestreLluvioso = new int[4];
+        for (int i = 0; i < registroDiasLluvia.length; i++) {
+            for (int j = 0; j < registroDiasLluvia[i].length; j++) {
+                if (registroDiasLluvia[i][j]) {
+                    int diaDelAño = j + 1;
+
+                    if (diaDelAño <= 90) {
+                        trimestreLluvioso[0]++;
+                    } else if (diaDelAño <= 180) {
+                        trimestreLluvioso[1]++;
+                    } else if (diaDelAño <= 270) {
+                        trimestreLluvioso[2]++;
+                    } else {
+                        trimestreLluvioso[3]++;
+                    }
+                }
+            }
+        }
+
+        int max = trimestreLluvioso[0];
+        int trimestreMasLluvioso = 1;
+        for (int i = 1; i < trimestreLluvioso.length; i++) {
+            if (trimestreLluvioso[i] > max) {
+                max = trimestreLluvioso[i];
+                trimestreMasLluvioso = i + 1;
+            }
+        }
+        return trimestreMasLluvioso;
+    }
 }
